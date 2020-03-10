4f450c2e76b92d0866023900cfd44d9162e33bbd
CarlosSantanaSP <61557079+CarlosSantanaSP@users.noreply.github.com>
2020-03-09 22:50:33 -0300

    O primeiro sketch do projeto apenas utilizando o sensor e fazendo uma comunicação simples

---
+++ prog1_noDB.ino
@@ -0,0 +1,18 @@
+int objeto = 0;
+int sensor = 7;
+
+void setup(){
+pinMode (sensor, INPUT);
+Serial.begin (9600);
+}
+
+void loop(){
+objeto = digitalRead(sensor);
+
+if (objeto == 0){
+Serial.println ("Vaga Ocupada");
+delay(1000);
+}
+else {
+Serial.println ("Vaga Livre);
+}
